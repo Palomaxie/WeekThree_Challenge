6c9083c935793a7f967fee00fdad189997d044b2
Pearl Paloma <pearlpaloma@gmail.com>
2019-02-21 12:56:42 +0300

    Sorting challenge

---
+++ list_sort.py
@@ -0,0 +1,24 @@
+
+def list_sort(lists):
+    dict_list = {'evens': [], 'odds': [], 'chars': []}
+    even = []
+    odd = []
+    char = []
+    for item in lists:
+        if type(item) == int and item % 2 == 0:
+            even.append(item)
+            dict_list['evens'] = even
+
+        elif type(item) == int and item % 2 != 0:
+            odd.append(item)
+            dict_list['odds'] = odd
+
+        elif type(item) == str:
+            char.append(item)
+            dict_list['chars'] = char
+
+    return dict_list
+
+
+print(list_sort([4, 9, 2, 3, 5, 1, 'd', 'a', 'c', 'f']))
+
