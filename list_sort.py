7b1a93f833b3c39ea0a6e50be5c275a09ea34d48
Pearl Paloma <pearlpaloma@gmail.com>
2019-02-25 10:52:25 +0300

    Added the sorting method and edited the list_sort_test.py

--- list_sort.py
+++ list_sort.py
@@ -7,15 +7,15 @@ def list_sort(lists):
     for item in lists:
         if type(item) == int and item % 2 == 0:
             even.append(item)
-            dict_list['evens'] = even
+            dict_list['evens'] = sorted(even)
 
         elif type(item) == int and item % 2 != 0:
             odd.append(item)
-            dict_list['odds'] = odd
+            dict_list['odds'] = sorted(odd)
 
         elif type(item) == str:
             char.append(item)
-            dict_list['chars'] = char
+            dict_list['chars'] = sorted(char)
 
     return dict_list
 
